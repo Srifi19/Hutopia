71946bfeb72ac7db72810d945cb6e4e0b9b9c59a
Srifi19 <109959950+Srifi19@users.noreply.github.com>
2023-09-26 08:26:59 +0300

    Update mainData.js

--- EnterpriseHutopia/api/Database/mainData.js
+++ EnterpriseHutopia/api/Database/mainData.js
@@ -1,7 +1,7 @@
 module.exports  = {
-    HOST : "172.32.21.1",
-    USERNAME: "samer",
-    PASSWORD: "S@m3r#2023",
-    DATABASE : "Hutopia_Final"
+    HOST : "null",
+    USERNAME: "null",
+    PASSWORD: "null",
+    DATABASE : "null" 
 }
 
